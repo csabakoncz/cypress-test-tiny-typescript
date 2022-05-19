2f85b446f51067a769573e86ddcd6be2587a84f3
Csaba Koncz <ckoncz@cloudera.com>
2022-05-19 16:25:04 +0200

    add spec.ts and webpack/typescript setup

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  resolve: {
+    extensions: ['.ts', '.js'],
+    modules: [
+      "node_modules",
+      "cypress/lib",
+      "cypress"
+    ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: [/node_modules/],
+        use: [{
+          loader: 'ts-loader',
+          options: {
+            transpileOnly: true // IMPORTANT! use transpileOnly mode to speed-up compilation
+          }
+        }],
+      },
+    ],
+  },
+}
